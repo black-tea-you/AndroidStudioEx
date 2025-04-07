162ba9f369acbfcac0e86821aa53f3759ad8bfbb
YOU SI HYUN <smtsnyout@gmail.com>
2025-04-08 01:19:34 +0900

    Version2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +19,7 @@ import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.net.Uri;
@@ -45,32 +48,66 @@ public class MainActivity extends AppCompatActivity {
     String[] items = {"mike", "angel", "crow", "john", "ginnie", "sally", "cohen", "rice"};
     LinearLayout mLinearLayout;
 
+
+    private boolean running;
+    private int value;
+
+    TextView textView0;
+    ProgressBar bar;
+    ProgressHandler handler;
+
+    Handler handler2;
+    ProgressRunnable runnable;
+    boolean isRunning=false;
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         EdgeToEdge.enable(this);
         
         
-        //animation 예제
-        mLinearLayout = new LinearLayout(this);
-        // Instantiate an ImageView and define its properties
-        ImageView img = new ImageView(this);
-        Drawable myImage = getResources().getDrawable(R.drawable.ic_launcher_foreground);
-        img.setImageDrawable(myImage);
-        // to match the Drawable's dimensions
-        img.setLayoutParams(new Gallery.LayoutParams(Gallery.LayoutParams.WRAP_CONTENT,
-                Gallery.LayoutParams.WRAP_CONTENT));
-        // Add the ImageView to the layout and set the layout as the content view
-        mLinearLayout.addView(img);
-        setContentView(mLinearLayout);
+//        //animation 예제
+//        mLinearLayout = new LinearLayout(this);
+//        // Instantiate an ImageView and define its properties
+//        ImageView img = new ImageView(this);
+//        Drawable myImage = getResources().getDrawable(R.drawable.ic_launcher_foreground);
+//        img.setImageDrawable(myImage);
+//        // to match the Drawable's dimensions
+//        img.setLayoutParams(new Gallery.LayoutParams(Gallery.LayoutParams.WRAP_CONTENT,
+//                Gallery.LayoutParams.WRAP_CONTENT));
+//        // Add the ImageView to the layout and set the layout as the content view
+//        mLinearLayout.addView(img);
+//        setContentView(mLinearLayout);
 
-        //setContentView(R.layout.activity_canvas);//parsing 해오기 위한 작업
+        setContentView(R.layout.activity_thread);//parsing 해오기 위한 작업
         //동적 작업 하는 것 확인 유니티에서 코드로 오브젝트 생성하는 것과 비슷
         //id를 R.java가 모두 모아서 관리한다
+
         Log.d(tag, "In the onCreate() event");
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.canvas), (v, insets) -> {
+        final TextView textview = (TextView) findViewById(R.id.textView01);
+        Button button_Thread=(Button)findViewById(R.id.button_thread);
+
+        button_Thread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview.setText("스레드에서 받은 값: " + value);
+            }
+        });
+
+        textView0 = (TextView) findViewById(R.id.TextView01);
+        bar = (ProgressBar) findViewById(R.id.progress);
+        handler = new ProgressHandler();
+
+        handler2 = new Handler();
+        runnable = new ProgressRunnable();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_Thread), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -177,9 +214,69 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    class MyThread extends Thread{
+
+        @Override
+        public void run() {
+            super.run();
+        }
+    }
+
+    class MyRunnableClass implements Runnable{
+
+        @Override
+        public void run() {
+
+        }
+    }
+
     public void onStart() {
         super.onStart();
+        bar.setProgress(0);
+        Thread thread1=new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    for(int i=0; i<20 &&isRunning; i++) {
+                        Thread.sleep(1000);
+                        Message msg=handler.obtainMessage();
+                        //handler.sendMessage(msg);
+                        handler.post(runnable);//Handler의 post() method는 runnable 객체내의 run() 메소드를 실행하게 함
+
+
+                    }
+                } catch (Exception ex) {
+                    Log.e("MainActivity","Exception in processing message.",ex);
+                }
+            }
+        });
+        isRunning = true;
+        thread1.start();
         Log.d(tag, "In the onStart() event");
+    }
+
+    
+    //두가지 다른 예제들
+    public class ProgressHandler extends Handler {
+        public void handleMessage(Message msg){
+            bar.incrementProgressBy(5);//5씩 게이지 바 값이 증가한다는 말
+            if(bar.getProgress()==bar.getMax()){
+                textView0.setText("Done");
+            } else {
+                textView0.setText("Working..."+bar.getProgress());
+            }
+        }
+    }
+
+    public class ProgressRunnable implements Runnable {
+        public void run(){
+            bar.incrementProgressBy(5);
+            if(bar.getProgress()==bar.getMax()){
+                textView0.setText("Done");
+            } else {
+                textView0.setText("Working..."+bar.getProgress());
+            }
+        }
     }
 
     public void onRestart() {
@@ -187,19 +284,37 @@ public class MainActivity extends AppCompatActivity {
         Log.d(tag, "In the onRestart() event");
     }
 
-    public void onResume() {
-        super.onResume();
-        Log.d(tag, "In the onResume() event");
-    }
 
-    public void onPause() {
-        Log.d(tag, "In the onPause() event");
+    //Thread 예제
+    @Override
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Thread thread1 = new BackgroundThread();
+        thread1.start();
+    }
+    @Override
+    protected void onPause() {
         super.onPause();
+        running = false;
+        value = 0;
+    }
+    class BackgroundThread extends Thread {
+        public void run(){
+            while (running){
+                try{
+                    Thread.sleep(1000);
+                    value++;
+                }
+                catch(Exception ex){}
+            }
+        }
     }
 
     public void onStop() {
         Log.d(tag, "In the onStop() event");
         super.onStop();
+        isRunning = false;
     }
 
     public void onDestroy() {
@@ -207,3 +322,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
     }
 }
+
